de6ac6f1e3013506145dd21778534f3ab76e9f3b
sujonaust2008 <sujonaust2008@gmail.com>
2022-05-26 23:53:20 +0600

    add service and delete service api added

--- index.js
+++ index.js
@@ -28,7 +28,20 @@ async function run(){
             const query={_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
-        })
+        });
+
+        app.post('/service', async(req, res) =>{
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        app.delete('/service/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
 
     }
     finally{
