ceb1fc7249ab09b1672d6a91db0cad01b54aeb4f
sujonaust2008 <sujonaust2008@gmail.com>
2022-05-26 15:56:35 +0600

    create api for product detail

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +21,13 @@ async function run(){
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/service/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
 
     }
